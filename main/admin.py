9913e02696ecf300d580ba41922e045c05936f70
RPR <rivaldy.rivly@gmail.com>
2026-03-29 20:17:47 +0700

    feat: complete OAuth2 authentication & authorization implementation

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SiteSettings
 
-# Register your models here.
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display  = ['primary_color', 'font_family', 'font_size_base', 'last_modified_by', 'updated_at']
+    readonly_fields = ['last_modified_by', 'updated_at']
